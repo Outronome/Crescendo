790e29a9653dcd4860bc63b3d22e0bafdef5d9a5
Rodrigo Barbosa <1241626@isep.ipp.pt>
2025-03-25 03:16:44 +0000

    Pagina de Marketplace Feita - Filtragem e Listagem (Front-End)

--- Crescendo/resources/views/pagina/market-place/index.blade.php
+++ Crescendo/resources/views/pagina/market-place/index.blade.php
@@ -1,17 +1,71 @@
-<div class=  "static bg-cover bg-center h-screen  bg-[#66c6ba]" >
-    <div class = " h-screen w-screen p-4">
-        <div class = "border border-black p-4 w-5/7 h-3/4 flex items-center justify-center mx-[50px] mt-28 bg-[#8bd4cb] rounded-[15px]">
-            <div class=" h-full w-1/4 border border-black p-4 flex-wrap  rounded-[15px]">
+<div class="static bg-cover bg-center h-screen  bg-[#66c6ba]">
+
+    <div class=" h-screen w-screen p-4">
+
+        <div class=" p-4 w-5/7 h-3/4 flex items-center justify-center mx-[50px] mt-28 bg-[#8bd4cb] rounded-[15px]">
+
+            <div class=" h-full w-1/4 border border-black p-4 flex flex-col  rounded-[15px]">
+
                 <h1 class="text-2xl font-bold">Filtros</h1>
-                
-            </div>
-                <div class=" flex-row w-full h-full grid grid-cols-5 gap-16  px-16  border border-black p-4 rounded-[15px] ml-[10px] justify-center ">
-                    <div class="border border-black  rounded-[15px] w-[200px] h-[250px] "></div>
-                    <div class="border border-black  rounded-[15px] w-[200px] h-[250px] "></div>
-                    <div class="border border-black  rounded-[15px] w-[200px] h-[250px] "></div>
-                    <div class="border border-black  rounded-[15px] w-[200px] h-[250px] "></div>
-                    <div class="border border-black  rounded-[15px] w-[200px] h-[250px] "></div>
+
+                <input
+                    class="  border border-black rounded-[5px] mt-[10px] pl-[5px] mb-[10px] bg-[#85b2bf]"
+                    type="text" placeholder="Nome da musica">
+
+                <h2 class="text-2xl font-bold mb-[10px]">Categoria</h2>
+
+                <select id="Categorias"
+                    class="bg-gray-50 border border-gray-300 text-[#8bd4cb] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+                dark:bg-[#85b2bf] dark:border-gray-600 dark:placeholder-black dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    <option class="text-black" selected>Escolha uma categoria</option>
+
+                    <option class="text-black" value="">Rock</option>
+
+                    <option class="text-black" value="">Pop</option>
+
+                    <option class="text-black" value="">Rap</option>
+
+                    <option class="text-black" value="">Fado</option>
+                </select>
+                <div>
+                    <h3 class="text-2xl font-bold mb-[10px] mt-[10px]">Preços</h3>
+
+                    <input class="w-1/5 ml-[50px] bg-[#85b2bf] " type="number" id="minValue" placeholder="0">
+
+                    <label class=" ml-[20px] mr-[20px] font-bold text-1xl" for="">Ate</label>
+
+                    <input class="w-1/5 bg-[#85b2bf] " type="number" id="maxValue" placeholder="200">
                 </div>
-        </div> 
+
+
+                <h4 class="text-2xl font-bold mb-[10px] mt-[10px]">Duração</h4>
+
+
+                <div class="flex items-center space-x-2">
+                    <input class="w-1/3 bg-[#85b2bf] text-center" type="time" id="timeInput" placeholder="">
+                    <button onclick="document.getElementById('timeInput').value = ''"
+                        class="px-3 py-2 bg-[#85b2bf] text-black rounded border border-black ">
+                        Limpar
+                    </button>
+                </div>
+                <button
+                    class="w-full border border-black rounded-[15px] mt-auto font-bold">Pesquisar</button>
+            </div>
+
+            <div
+                class=" flex-row w-full h-full grid grid-cols-5 gap-16  px-16  border border-black p-4 rounded-[15px] ml-[10px] justify-center ">
+                <div class="bg-[#85b2bf] rounded-[15px] w-[200px] h-[250px] ">
+                    <div class="w-full h-2/3 border border-black rounded-tr-[15px] rounded-tl-[15px] overflow-hidden">
+                        <img class="w-full h-full object-cover" src="/assets/img/271.jpg" alt="">
+                    </div>
+
+                </div>
+                <div class="bg-[#85b2bf] rounded-[15px] w-[200px] h-[250px] "></div>
+                <div class="bg-[#85b2bf] rounded-[15px] w-[200px] h-[250px] "></div>
+                <div class="bg-[#85b2bf] rounded-[15px] w-[200px] h-[250px] "></div>
+                <div class="bg-[#85b2bf] rounded-[15px] w-[200px] h-[250px] "></div>
+            </div>
+        </div>
     </div>
 </div>
--- Crescendo/tailwind.config.js
+++ Crescendo/tailwind.config.js
@@ -1,33 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-    ],
-    theme: {
-      extend: {},
-      screens:{
-        'telemovelMI':'320px',
-        'telemovelMF':'375px',
-        'telemovelGI':'376px',
-        'telemovelGF':'425px',
-        'tabletI':'426px',
-        'tabletF':'768px',
-        'ecraPPI':'769px',
-        'ecraPPF':'1024px',
-        'ecraPI':'1025px',
-        'ecraPF':'1440px',
-        'ecraMI':'1441px',
-        'ecraMF':'2560px',
-        'ecraG':'2561px',
-      }
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: "#8bd4cb",
+      },
+      screens: {
+        'telemovelMI': '320px',
+        'telemovelMF': '375px',
+        'telemovelGI': '376px',
+        'telemovelGF': '425px',
+        'tabletI': '426px',
+        'tabletF': '768px',
+        'ecraPPI': '769px',
+        'ecraPPF': '1024px',
+        'ecraPI': '1025px',
+        'ecraPF': '1440px',
+        'ecraMI': '1441px',
+        'ecraMF': '2560px',
+        'ecraG': '2561px',
+      },
     },
-    plugins: [
-      require('flowbite/plugin')
-    ],
-
-    
-  }
-
-  
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
