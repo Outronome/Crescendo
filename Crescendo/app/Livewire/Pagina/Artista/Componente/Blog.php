540fcd030e5705b051020bfc7c8d1f3f505f8f86
Rafael Ferraria <1241623@isep.ipp.pt>
2025-03-27 02:52:04 +0000

    Avanço Página Artista

---
+++ Crescendo/app/Livewire/Pagina/Artista/Componente/Blog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Artista\Component;
+
+use Livewire\Component;
+
+class Blog extends Component
+{
+    public function render()
+    {
+        return view('pagina.artista.componente.blog');
+    }
+}
